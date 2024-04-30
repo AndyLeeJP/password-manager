e297dda3d7a6e015967c13bdd9271ec19f66bac3
vaperLee <rize1126jesse@gmail.com>
2024-04-30 15:38:29 +0900

    implement add function

--- password_manager.py
+++ password_manager.py
@@ -1,15 +1,20 @@
 master_pwd = input("What is the master password？")
 
 def view():
-    pass
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            website, userId, password = data.split("|")
+            print("Website or app: ", website, "| User name or ID: ", userId, "| Password: ", password)
+
 
 def add():
-    name = input('Account name: ')
+    name = input('website or app name: ')
+    user_id = input('user name or ID: ')
     pwd = input("Password: ")
 
     with open('passwords.txt', 'a') as f:
-        f.write(name + "|" + pwd)
-
+        f.write(name + "|" + user_id + "|" + pwd + "\n")
 
 while True:
     mode = input("Would you like to add a new password ot view existing ones (view, add)?, press q to quit ").lower()
